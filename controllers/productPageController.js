74f5c2cbe555f10fda7bdf0d854dc26ad2c8e1ac
İbrahim Ekmen <ibrahimekmentr70@gmail.com>
2021-08-23 15:24:16 +0300

    navbar updated

--- controllers/productPageController.js
+++ controllers/productPageController.js
@@ -2,18 +2,42 @@ const productServices = require('../services/productServices.js');
 
 function render(req,res){
     const { productName } = req.params;
+    const { category } = req.params;
+    let gender = category.split("-")[0];
+    gender = gender.replace("s","");
+    gender = gender[0].toUpperCase() + gender.slice(1);
+
+    const results = differentiateGender(req,gender);
+    const categories = results[0];
+    const subCategories = results[1];
+    
     productServices.getProductByName(productName).then(data =>{
         res.render('product',{
-            gender: "Men",
+            gender: gender,
             product: data[0],    
             breadcrumbs: req.breadcrumbs,
-            categories: req.menNavbar[0],
-            subcategories: req.menNavbar[1]
+            categories: categories,
+            subcategories: subCategories
         });
     }).catch(err=>{
         console.log(err);
     });
 }
+
+function differentiateGender(req,gender){
+    var categories;
+    var subCategories;
+    if(gender === "Men"){
+        categories = req.menNavbar[0];
+        subCategories = req.menNavbar[1];
+    }else{
+        categories = req.womenNavbar[0];
+        subCategories = req.womenNavbar[1];
+    }
+    var results = [categories,subCategories];
+    return results;
+}
+
 
 module.exports = {
     render
